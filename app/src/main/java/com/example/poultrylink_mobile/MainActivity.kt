3a0d44ca4ce7d19d6d90b683efb2f9adf3674dd1
Yashin Al Fauzy <yashinfauzy@apps.ipb.ac.id>
2024-10-17 12:39:53 +0700

    test

--- app/src/main/java/com/example/poultrylink_mobile/MainActivity.kt
+++ app/src/main/java/com/example/poultrylink_mobile/MainActivity.kt
@@ -15,6 +15,6 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+        } //test yashin
     }
 }
